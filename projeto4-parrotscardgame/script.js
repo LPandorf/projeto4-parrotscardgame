9b19bba7136f06b453deace223328852e02577db
LPandorf <106892978+LPandorf@users.noreply.github.com>
2022-06-20 17:30:07 -0300

    Add files via upload

--- projeto4-parrotscardgame/script.js
+++ projeto4-parrotscardgame/script.js
@@ -1,7 +1,9 @@
 (function(){
+    var matches=0;
     var images=[];
     var flippedCards=[];
     var modalGameOver = document.querySelector("#gameover");
+    var imgMatchSign = document.querySelector("#imgMatchSign");
 
     for(var i = 1; i < 15; i++){
         var img = {
@@ -22,6 +24,7 @@
         var frontFaces = document.getElementsByClassName("front");
 
         for( var i = 0; i<14; i++){
+            
             var card = document.querySelector("#card"+i);
             card.style.left = i % 7 === 0 ? 5 +"px" : i % 7*165+5+"px";
             card.style.top = i < 7 ? 5 + "px": 250 + "px";
@@ -31,6 +34,10 @@
             frontFaces[i].setAttribute("id", images[i].id);
             
         }
+
+        modalGameOver.style.zIndex=-2;
+        modalGameOver.removeEventListener("click",startGame,false);
+
     }
 
     function randomSort(oldArray){
@@ -59,6 +66,26 @@
             faces[1].classList.toggle("flipped");
 
             flippedCards.push(this);
+
+            if(flippedCards.length===2){
+                if(flippedCards[0].childNodes[3].id=== flippedCards[1].flippedCards[3].id){
+                    flippedCards[0].childNodes[1].classList.toggle("match");
+                    flippedCards[0].childNodes[3].classList.toggle("match");
+                    flippedCards[1].childNodes[1].classList.toggle("match");
+                    flippedCards[1].childNodes[3].classList.toggle("match");
+
+                    matchCardSing();
+
+                    matches++;
+
+                    flippedCards= [];
+
+                    if(matches===7){
+                        gameOver();
+                    }
+
+                }
+            }
         } else{
             flippedCards[0].childNodes[1].classList.toggle("flipped");
             flippedCards[0].childNodes[3].classList.toggle("flipped");
@@ -72,7 +99,19 @@
     
 
     function gameOver(){
-        modalGameOver.style.zIndex=10
+        modalGameOver.style.zIndex=10;
+        modalGameOver.addEvenListener("click",startGame,false);
     }
 
-}());
+    function matchCardSign(){
+        imgMatchSign.style.zIndex=1;
+        imgMatchSign.style.top= 150+"px";
+        imgMatchSign.style.opacity=0;
+        setTimeout(function(){
+            imgMatchSign.style.zIndex=-1;
+            imgMatchSign.style.top= 250+"px";
+            imgMatchSign.style.opacity=1;
+        },1500);
+    }
+
+}())
